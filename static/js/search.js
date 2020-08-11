9274df3d05ae306357c9676acfa1972c70ae14bd
brentvollebregt <brentvollebregt@users.noreply.github.com>
2020-08-11 04:56:26 +0000

    deploy: e299f23ebd67c629739b1ff06473bbdc1e671d2e

--- static/js/search.js
+++ static/js/search.js
@@ -1,1 +1,21 @@
-function doSearch(inputElement){window.location.href=searchUrl+'?q='+encodeURIComponent(inputElement.value);};const sidebarSearchInput=document.getElementById('sidebar-search');const sidebarSearchButton=document.getElementById('sidebar-search-submit');const searchPageSearchInput=document.getElementById('search');sidebarSearchButton.addEventListener('click',function(){doSearch(sidebarSearchInput);});sidebarSearchInput.addEventListener('keyup',function(e){e.preventDefault();if((e.keyCode?e.keyCode:e.which)===13){doSearch(sidebarSearchInput);}});searchPageSearchInput.addEventListener('keyup',function(e){e.preventDefault();if((e.keyCode?e.keyCode:e.which)===13){doSearch(searchPageSearchInput);}});
+function doSearch(inputElement) {
+    window.location.href = searchUrl + '?q=' + encodeURIComponent(inputElement.value);
+}
+
+const sidebarSearchInput = document.getElementById('sidebar-search');
+const sidebarSearchButton = document.getElementById('sidebar-search-submit');
+const searchPageSearchInput = document.getElementById('search');
+
+sidebarSearchButton.addEventListener('click', function () { doSearch(sidebarSearchInput); });
+sidebarSearchInput.addEventListener('keyup', function (e) {
+    e.preventDefault();
+    if ((e.keyCode ? e.keyCode : e.which) === 13) {
+        doSearch(sidebarSearchInput);
+    }
+});
+searchPageSearchInput.addEventListener('keyup', function (e) {
+    e.preventDefault();
+    if ((e.keyCode ? e.keyCode : e.which) === 13) {
+        doSearch(searchPageSearchInput);
+    }
+});
